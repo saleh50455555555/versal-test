e55c4b257160559e7899781a020fba69e7f29151
saleh <sabwlwy99991@gmail.com>
2024-08-06 13:43:39 +0300

    myproject57

--- src/models/commentModel.js
+++ src/models/commentModel.js
@@ -2,12 +2,19 @@ const mongoose = require('mongoose');
 const User = require('../models/userModel');
 const TouristPlace = require('../models/touristplaceModel');
 
+
+const timee = () => {
+    const now = new Date;
+    now.setSeconds(0, 0);
+    return now;
+}
+
 const commentSchema = new mongoose.Schema({
     commentID: Number,
     placeID: { type: mongoose.Schema.Types.Number, ref: 'TouristPlace' },
     userID: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
     commentText: String,
-    timestamp: { type: Date, default: Date.now }
+    timestamp: { type: Date, default: timee }
 });
 
 
